6c30e595e304676aeac041133650100079db9f9c
weinventyou <ZachRScott@gmail.com>
2015-01-19 23:50:12 -0500

    Update config.js
    Split into two episodes of 5 stories each.

--- javascript/config.js
+++ javascript/config.js
@@ -5,7 +5,12 @@ var Config = {
   channels: [
     {
       title: "EP1",
-      images: ["1.mov", "2.mov", "3.mov", "4.mov", "5.mov", "6.mov", "7.mov", "8.mov", "9.mov"]
+      images: ["1.mov", "2.mov", "3.mov", "4.mov", "5.mov"]
+    },
+	
+	 {
+      title: "EP2",
+      images: ["6.mov", "7.mov", "8.mov", "9.mov", "10.mov"]
     },
     
   
